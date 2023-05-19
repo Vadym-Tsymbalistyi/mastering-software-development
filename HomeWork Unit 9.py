e4c382272a41bb0d9f3703966dbdb48945c32914
Vadym-Tsymbalistyi <vadumcimbalist@gmail.com>
2023-05-19 09:09:11 +0200

    Done unit 9 function

--- HomeWork Unit 9.py
+++ HomeWork Unit 9.py
@@ -1,16 +1,22 @@
-#9.1  Define a function called good() that returns the list ['Harry', 'Ron', 'Hermione']
+# 9.1  Define a function called good() that returns the list ['Harry', 'Ron', 'Hermione']
+
 def good():
-    return ['Harry','Ron','Hermione']
+    return ['Harry', 'Ron', 'Hermione']
+
 
 print(good())
 
-# Help me (9,2; 9,3;)!!!!!!! ----Google helped...
 
-#9.2
-#Define a generator function called get_odds that returns the odd numbers from
+# 9.2
+# Define a generator function called get_odds that returns the odd numbers from
 # range(10). Use a for loop to find and print the third value returned.
-limit = 10
-get_odds = (num for num in range(limit) if not num % 2 == 0)
+
+def get_odds():
+    yield num
+
+
+get_odds = (num for num in range(10) if not num % 2 == 0)
+
 count = 0
 for num in get_odds:
     if count == 2:
@@ -19,48 +25,40 @@ for num in get_odds:
     count += 1
 
 
-
-# def get_odds():
-#     for number in range(1, 10, 2):
-#         yield number
-
-# count = 1
-# for number in get_odds():
-#     if count == 3:
-#         print("The third odd number is", number)
-#         break
-#     count += 1
-
-#9.3
+# 9.3
 #  Define a decorator called test that prints 'start' when a function is called and
 # 'end' when it finishes.
 
-
-def test(func):
-    def nested_function(*args, **kwargs):
+def decorator(func):
+    def nested_function(a, b):
         print('start')
-        result = func(*args, **kwargs)
+        func(a, b)
         print('end')
-        return result
+
     return nested_function
 
-@test
+
+@decorator
 def add(a, b):
     print(a + b)
 
-add(12,4)
+
+add(12, 4)
+
 
 # 9.4 Define an exception called OopsException. Raise this exception to see what hap‐
 # pens. Then write the code to catch this exception and print 'Caught an oops'.
 
-
 class OopsException(Exception):
     pass
+
+
 def with_exception(a):
     if a < 0:
         raise OopsException(a)
+
+
 try:
     with_exception(-1)
 except OopsException as exc:
     print('Caught an oops')
-#1
