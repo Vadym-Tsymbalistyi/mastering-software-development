0390d694e8fe0b5efae116f2838f448702a4a98a
Vadik <131210395+Vadym-Tsymbalistyi@users.noreply.github.com>
2023-06-10 15:56:53 +0200

    Unit 9 function

--- Introducing Python/HomeWork Unit 9.py
+++ Introducing Python/HomeWork Unit 9.py
@@ -1,4 +1,4 @@
-# 9.1  Define a function called good() that returns the list ['Harry', 'Ron', 'Hermione']
+# 9.1.  Define a function called good() that returns the list ['Harry', 'Ron', 'Hermione']
 
 def good():
     return ['Harry', 'Ron', 'Hermione']
