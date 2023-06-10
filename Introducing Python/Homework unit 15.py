b35c38dfd6354453750e872957075bf568aa8382
Vadik <131210395+Vadym-Tsymbalistyi@users.noreply.github.com>
2023-06-10 16:05:57 +0200

    Chapter 15: : Data in Time: Processes and Concurrency

--- Introducing Python/Homework unit 15.py
+++ Introducing Python/Homework unit 15.py
@@ -1,4 +1,4 @@
-# 15.1 Use multiprocessing to create three separate processes. Make each one wait a
+# 15.1. Use multiprocessing to create three separate processes. Make each one wait a
 # random number of seconds between zero and one, print the current time, and then
 # exit.
 import multiprocessing
