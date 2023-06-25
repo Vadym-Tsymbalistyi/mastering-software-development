b9e8f415f9c371f9697e3edf8aa0e5f5215358c5
Vadik <131210395+Vadym-Tsymbalistyi@users.noreply.github.com>
2023-06-25 13:15:07 +0200

    Chapter 17:Data in Space: Networks

---
+++ Introducing Python/Homework unit 17.1 client.py
@@ -0,0 +1,13 @@
+# 17.1 Use a plain socket to implement a current-time-service. When a client sends the
+# string time to the server, return the current date and time as an ISO string.
+import socket
+from datetime import datetime
+
+address = ('localhost', 6789)
+max_size = 4096
+print('Starting the client at', datetime.now())
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.sendto(b'time', address)
+data, server = client.recvfrom(max_size)
+print('Client read', data)
+client.close()
