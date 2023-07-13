06b6090198f86248e264c2b9692a427bc0240ea4
Vadik <vadumcimbalist@gmail.com>
2023-07-13 12:37:30 +0200

    17

--- Introducing Python/Homework unit 17.1 client.py
+++ Introducing Python/Homework unit 17.1 client.py
@@ -1,13 +1,26 @@
 # 17.1 Use a plain socket to implement a current-time-service. When a client sends the
 # string time to the server, return the current date and time as an ISO string.
+#import socket
+#from datetime import datetime
+#
+#address = ('localhost', 6789)
+#max_size = 4096
+#print('Starting the client at', datetime.now())
+#client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#client.sendto(b'time', address)
+#data, server = client.recvfrom(max_size)
+#print('Client read', data)
+#client.close()
 import socket
 from datetime import datetime
-
+from time import sleep
 address = ('localhost', 6789)
 max_size = 4096
 print('Starting the client at', datetime.now())
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto(b'time', address)
-data, server = client.recvfrom(max_size)
-print('Client read', data)
-client.close()
+while True:
+    sleep(5)
+    client.sendto(b'time', address)
+    data, server_addr = client.recvfrom(max_size)
+    print('Client read', data)
+    client.close()
