4c95d0cdae661d425978340ce0da9fca17149e65
Vadik <vadumcimbalist@gmail.com>
2023-09-23 16:59:33 +0200

    Chapter 4 sorting algorithms and test

---
+++ Computer_Science_dlya_programmista-Samouchki/insertion_sort_test.py
@@ -0,0 +1,13 @@
+from unittest import TestCase, main
+from Chapter_4_Sorting_Algorithms import insertion_sort
+
+
+class InsertionSortTest(TestCase):
+    def insertion_sort_test(self):
+        a_list = [3, 2, 4, 567, 7, 8, 7, 445, 34]
+        result = [2, 3, 4, 7, 7, 8, 34, 445, 567]
+        self.assertEquals(insertion_sort(a_list), result)
+
+
+if __name__ == '__main__':
+    main()
