262ed972f48e5d79f933f4582a14696505801f5f
Vadik <vadumcimbalist@gmail.com>
2023-09-29 10:38:52 +0200

    Chapter 5 sorting algorithms and test

--- Computer_Science_dlya_programmista-Samouchki/Chapter_4_Sorting_Algorithms.py
+++ Computer_Science_dlya_programmista-Samouchki/Chapter_4_Sorting_Algorithms.py
@@ -10,44 +10,27 @@ def insertion_sort(a_list):
         a_list[i] = value
     return a_list
 
-
-a_list = [3, 2, 4, 567, 7, 8, 7, 445, 34]
-print('SOURCE ARRAY', a_list)
-result = insertion_sort(a_list)
-print('Sort result', result)
-
-# Cортировки слиянием
-# def merge_sort(lst):
-#    if len(lst) <= 1:
-#        return lst
-#    middle = len(lst) // 2
-#    left = lst[:middle]
-#    right = lst[middle:]
-#
-#    left = merge_sort(left)
-#    right = merge_sort(right)
-#
-#    return list(merge(left, right))
-#
-#
-# def merge(left, right):
-#    res = []
-#    while len(left) != 0 and len(right) != 0:
-#        if left[0] < right[0]:
-#            res.append(left[0])
-#            left.remove(left[0])
-#        else:
-#            res.append(right[0])
-#            right.remove(right[0])
-#    if len(left) == 0:
-#        res += right
-#    else:
-#        res += left
-#    return res
-#
-#
-# lst = [2, 342, 5, 444, 75, 85, 68, 5, 85, 85, 8, 777]
-# print('SOURCE ARRAY', lst)
-# result = merge_sort(lst)
-# print('Sort result', result)
-#
+#Cортировки слиянием
+def merge_sort(lst):
+   if len(lst) <= 1:
+       return lst
+   middle = len(lst) // 2
+   left = lst[:middle]
+   right = lst[middle:]
+   left = merge_sort(left)
+   right = merge_sort(right)
+   return list(merge(left, right))
+def merge(left, right):
+   res = []
+   while len(left) != 0 and len(right) != 0:
+       if left[0] < right[0]:
+           res.append(left[0])
+           left.remove(left[0])
+       else:
+           res.append(right[0])
+           right.remove(right[0])
+   if len(left) == 0:
+       res += right
+   else:
+       res += left
+   return res
--- Computer_Science_dlya_programmista-Samouchki/insertion_sort_test.py
+++ Computer_Science_dlya_programmista-Samouchki/insertion_sort_test.py
@@ -3,7 +3,7 @@ from Chapter_4_Sorting_Algorithms import insertion_sort
 
 
 class InsertionSortTest(TestCase):
-    def insertion_sort_test(self):
+    def test_insertion_sort(self):
         a_list = [3, 2, 4, 567, 7, 8, 7, 445, 34]
         result = [2, 3, 4, 7, 7, 8, 34, 445, 567]
         self.assertEquals(insertion_sort(a_list), result)
