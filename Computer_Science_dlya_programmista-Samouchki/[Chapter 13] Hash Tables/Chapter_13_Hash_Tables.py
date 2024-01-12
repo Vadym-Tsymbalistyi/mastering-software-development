0534205c75b46250ef59196283f06793984242c3
Vadik <vadumcimbalist@gmail.com>
2024-01-12 19:18:11 +0200

    def remove_duplicate_words and unittest (Hash Tables) done

---
+++ Computer_Science_dlya_programmista-Samouchki/[Chapter 13] Hash Tables/Chapter_13_Hash_Tables.py
@@ -0,0 +1,15 @@
+def remove_duplicate_words(sentence):
+    words = sentence.split()
+    unique_words = set()
+    new_sentence = []
+
+    for word in words:
+        if word not in unique_words:
+            unique_words.add(word)
+            new_sentence.append(word)
+    return " ".join(new_sentence)
+
+
+original_sentence = "I am a self-taught programmer looking for a job as a programmer ."
+result = remove_duplicate_words(original_sentence)
+print(result)
