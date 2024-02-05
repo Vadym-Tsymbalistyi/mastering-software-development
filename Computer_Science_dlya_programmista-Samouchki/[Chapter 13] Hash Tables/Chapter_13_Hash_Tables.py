9db34b44e6d41d8279c65c8d791cecfddddd807d
Vadik <vadumcimbalist@gmail.com>
2024-02-05 10:02:11 +0100

    FIXET hash tables

--- Computer_Science_dlya_programmista-Samouchki/[Chapter 13] Hash Tables/Chapter_13_Hash_Tables.py
+++ Computer_Science_dlya_programmista-Samouchki/[Chapter 13] Hash Tables/Chapter_13_Hash_Tables.py
@@ -3,7 +3,6 @@
 # Ваша функция должна вернуть "I am a self-taught programmer looking for
 # a job as a.".
 from collections import OrderedDict
-#import string
 
 
 def remove_dots(del_dots):
@@ -11,19 +10,12 @@ def remove_dots(del_dots):
 
 
 def remove_duplicate_words(sentence):
-    #translator = str.maketrans('', '', string.punctuation)
     words = sentence.split()
     unique_words = OrderedDict()
 
     for word in words:
-        #cleaned_word = word.translate(translator)
         cleaned_word = remove_dots(word)
         unique_words[cleaned_word] = None
 
     new_sentence = list(unique_words.keys())
     return " ".join(new_sentence)
-
-
-sentence = "I am a self-taught programmer looking for a job as a programmer."
-result = remove_duplicate_words(sentence)
-print(result)
