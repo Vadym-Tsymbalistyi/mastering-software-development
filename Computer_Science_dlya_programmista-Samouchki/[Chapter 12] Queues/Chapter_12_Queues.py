d823d3d2e5a42c039cbc9bf1ca0629fed201bf63
Vadik <vadumcimbalist@gmail.com>
2024-02-05 09:59:31 +0100

    Fixet unit test AND private STECK

--- Computer_Science_dlya_programmista-Samouchki/[Chapter 12] Queues/Chapter_12_Queues.py
+++ Computer_Science_dlya_programmista-Samouchki/[Chapter 12] Queues/Chapter_12_Queues.py
@@ -2,17 +2,17 @@
 # постановки в очередь была равна О(1).
 class Queue:
     def __init__(self):
-        self.s1 = []
-        self.s2 = []
+        self.__s1 = []
+        self.__s2 = []
 
     def enqueue(self, item):  # константное время O(1),
-        self.s1.append(item)
+        self.__s1.append(item)
 
     def dequeue(self):  # амортизированное время O(1)
-        if not self.s1 and not self.s2:
+        if not self.__s1 and not self.__s2:
             raise Exception("Cannot pop from empty queue")
-        if not self.s2:
-            while self.s1:
-                self.s2.append(self.s1.pop())
+        if not self.__s2:
+            while self.__s1:
+                self.__s2.append(self.__s1.pop())
 
-        return self.s2.pop()
+        return self.__s2.pop()
