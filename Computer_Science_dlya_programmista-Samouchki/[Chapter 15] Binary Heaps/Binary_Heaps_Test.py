d264db0a375bd59a9b5af15fdf3f1598d17cf3b2
Vadik <vadumcimbalist@gmail.com>
2024-02-10 11:34:44 +0100

    done unittest BinaryHeaps

---
+++ Computer_Science_dlya_programmista-Samouchki/[Chapter 15] Binary Heaps/Binary_Heaps_Test.py
@@ -0,0 +1,29 @@
+from unittest import TestCase, main
+from Chapter_15_Binary_Heaps import BinaryTree, min_heap
+
+
+class TestBinaryTree(TestCase):
+    def test_min_heaps_true(self):
+        self.root = BinaryTree(2)
+        self.root.left = BinaryTree(3)
+        self.root.right = BinaryTree(4)
+        self.root.left.left = BinaryTree(5)
+        self.root.right.right = BinaryTree(6)
+        self.result = min_heap(self.root)
+        self.assertTrue(self.result)
+
+    def test_min_heaps_false(self):
+        self.root = BinaryTree(2)
+        self.root.left = BinaryTree(3)
+        self.root.right = BinaryTree(1)
+        self.result = min_heap(self.root)
+        self.assertFalse(self.result)
+
+    def test_heaps_empty_tree(self):
+        self.root = None
+        self.result = min_heap(self.root)
+        self.assertTrue(self.result)
+
+
+if __name__ == '__main__':
+    main()
