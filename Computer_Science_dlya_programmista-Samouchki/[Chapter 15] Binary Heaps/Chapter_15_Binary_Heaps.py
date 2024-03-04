f62863949e03a170eba7c53c4eb31246685131ea
Vadik <vadumcimbalist@gmail.com>
2024-03-04 11:00:08 +0100

    fixet  code

--- Computer_Science_dlya_programmista-Samouchki/[Chapter 15] Binary Heaps/Chapter_15_Binary_Heaps.py
+++ Computer_Science_dlya_programmista-Samouchki/[Chapter 15] Binary Heaps/Chapter_15_Binary_Heaps.py
@@ -14,12 +14,3 @@ def min_heap(node):
     if (node.left is not None and node.left.key < node.key) or (node.right is not None and node.right.key < node.key):
         return False
     return min_heap(node.left) and min_heap(node.right)
-
-
-root = BinaryTree(1)
-root.left = BinaryTree(2)
-root.right = BinaryTree(3)
-root.left.left = BinaryTree(4)
-root.right.right = BinaryTree(5)
-result = min_heap(root)
-print(result)
