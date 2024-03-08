a067fa1ed0ed82e9a8e05f062d7e1b07ba1491ab
Vadik <vadumcimbalist@gmail.com>
2024-03-08 11:23:56 +0100

    rename + fixet code + soled the  problem sqlalchemy (obfects of type)

--- Introducing Python/[Chapter 16] Data in a box trusted storage/Chapter_16_Data_in_a_box_trusted_storage.py
+++ Introducing Python/[Chapter 16] Data in a box trusted storage/Chapter_16_Data_in_a_box_trusted_storage.py
@@ -5,21 +5,17 @@
 # J R R Tolkien,The Hobbit
 # Lynne Truss,"Eats, Shoots & Leaves"
 import csv
-from turtle import title
-
-import select
-from sqlalchemy import engine
 
 text = ''''author,book
 J R R Tolkien,The Hobbit
 Lynne Truss,"Eats, Shoots & Leaves"'''
-with open('../books.csv', 'w') as outfile:
+with open('books.csv', 'w') as outfile:
     outfile.write(text)
 
 # 16.2 Use the csv module and its DictReader method to read books2.csv to the variable
 # books. Print the values in books. Did DictReader handle the quotes and commas in
 # the second bookвЂ™s title?
-with open('../books2.csv', 'r') as infile:
+with open('books2.csv', 'r') as infile:
     books = csv.DictReader(infile)
     for book in books:
         print(book)
@@ -31,21 +27,20 @@ with open('../books2.csv', 'r') as infile:
 # Thud!,Terry Pratchett,2005
 # The Spellman Files,Lisa Lutz,2007
 # Small Gods,Terry Pratchett,1992
-
 text = '''title,author,year
 The Weirdstone of Brisingamen,Alan Garner,1960
 Perdido Street Station,China MiГ©ville,2000
 Thud!,Terry Pratchett,2005
 The Spellman Files,Lisa Lutz,2007
 Small Gods,Terry Pratchett,1992'''
-with open('../books2.csv', 'w', encoding="utf-8") as outfile:
+with open('books2.csv', 'w', encoding="utf-8") as outfile:
     outfile.write(text)
 
 # 16.4 Use the sqlite3 module to create a SQLite database called books.db and a table
 # called books with these fields: title (text), author (text), and year (integer).
 import sqlite3
 
-db = sqlite3.connect('../books.db')
+db = sqlite3.connect('books.db')
 cursor = db.cursor()
 cursor.execute('''CREATE TABLE  IF NOT EXISTS books (title text, author text, year integer)''')
 db.commit()
@@ -54,11 +49,12 @@ db.commit()
 import csv
 
 int_str = '''insert into books values(?,?,?)'''
-with open('../books2.csv', 'r') as infile:
+with open('books2.csv', 'r') as infile:
     books = csv.DictReader(infile)
     for book in books:
         cursor.execute(int_str, (book['title'], book['author'], book['year']))
 db.commit()
+
 # 16.6 Select and print the title column from the book table in alphabetical order.
 sql = 'select title from books order by title asc'
 for row in db.execute(sql):
@@ -71,62 +67,27 @@ for row in db.execute('select*from books order by year'):
 # 16.8 Use the sqlalchemy module to connect to the sqlite3 database books.db that you
 # just made in exercise 16.4. As in 16.6, select and print the title column from the
 # book table in alphabetical order.
-# from sqlalchemy import create_engine
-#
-# engine = create_engine("sqlite:///books.db")
-# engine.connect()
-# if __name__ == '__main__':
-#    words = ['select title from books order by title asc']
-#
-#    words.sort()
-#    print(books.db)
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import sqlalchemy
 
-DATABASE_NAME = 'books.db'
+engine = sqlalchemy.create_engine('sqlite:///books.db')
+connection = engine.connect()
+sql = sqlalchemy.text('SELECT title FROM books ORDER BY title ASC')
+rows = connection.execute(sql)
 
-engine = create_engine(f'sqlite:///{DATABASE_NAME}')
-Session = sessionmaker(bind=engine)
-
-# Base = declarative_base()
-
-
-# def create_db():
-#    Base.metadata.create_all(engine)
-import os
-
-from sqlalchemy import and_, or_
-from sqlalchemy.orm import Session as SQLSession
-
-#if __name__ == '__main__':
-#    db_is_created = os.path.exists(DATABASE_NAME)
-#    print(select title)
-
-# with engine.connect() as connection:
-#    query = select([book.c.title]).order_by(book.c.title)
-# result = connection.execute(query)
-#
-# for row in result:
-#    print(row)
+for row in rows:
+    print(row.title)
 
 # 16.9 Install the Redis server and the Python redis library (pip install redis) on
 # your computer. Create a Redis hash called test with the fields count (1) and name
 # ('Fester Bestertester'). Print all the fields for test.
-# import redis
-#
-# conn = redis.Redis()
-# conn.delete('test')
-#
-# setName = 'test'
-#
-# nameAsKey = 'Fester Bestertester'
-# conn.hset(setName, nameAsKey, 0)
-#
-# conn.hgetall(setName)
-# print(conn.hgetall(setName))
-## 16.10 Increment the count field of test and print it.
-# conn.hincrby(setName, nameAsKey, 3)
-# conn.hget(setName, nameAsKey)
-# print(conn.hgetall(setName))
-#
+import redis
+
+client = redis.StrictRedis(host='localhost', port=8080, db=0)
+client.hset('test', 'count', 1)
+client.hset('test', 'name', 'Fester Bestertester')
+
+print(client.hgetall('test'))
+
+# 16.10 Increment the count field of test and print it.
+client.hincrby('test', 'count', 1)
+print(client.hget('test', 'count'))
