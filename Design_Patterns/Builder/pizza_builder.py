17397d08ec3a7dd05c628f0d72919e2cf08df166
Vadik <vadumcimbalist@gmail.com>
2024-04-17 12:40:00 +0200

    upgrade pizza_builder.py, add import abstractmethod , code is divided into parts (strategy,director)

--- Design_Patterns/Builder/pizza_builder.py
+++ Design_Patterns/Builder/pizza_builder.py
@@ -1,21 +1,25 @@
 from Design_Patterns.Director.director import Director
+from Design_Patterns.Strategy.strategy import DoughFirstStrategy, FillingFirstStrategy
+from abc import ABC, abstractmethod
 
 
-class PizzaBuilder:
-    def __init__(self):
-        self.pizza = Pizza()
-
+class PizzaBuilder(ABC):
+    @abstractmethod
     def build(self):
-        return self.pizza
+        return self
 
+    @abstractmethod
     def add_dough(self):
         return self
 
+    @abstractmethod
     def add_filling(self):
         return self
 
 
 class SalamiPizzaBuilder(PizzaBuilder):
+    def __init__(self, strategy):
+        self.pizza = Pizza(strategy)
 
     def add_dough(self):
         self.pizza.dough = 'Middle dough'
@@ -25,21 +29,33 @@ class SalamiPizzaBuilder(PizzaBuilder):
         self.pizza.filling = ['Salami', 'Tomatoes', 'Cheese']
         return self
 
+    def build(self):
+        return self.pizza
+
 
 class Pizza:
-    def __init__(self):
+    def __init__(self, strategy):
         self.dough = None
         self.filling = []
+        self.strategy = strategy
 
     def result(self):
+        print('Name strategy:', self.strategy.name())
+        print('Pizza strategy:', self.strategy.pizza_strategy(self))
         print('Pizza dough:', self.dough)
-        print("FilLing:", ",".join(self.filling))
+        print("Filling:", ",".join(self.filling))
 
 
 director = Director()
-builder = SalamiPizzaBuilder()
+builder = SalamiPizzaBuilder(FillingFirstStrategy())
 director.set_builder(builder)
 director.construct_pizza()
-pizza = builder.pizza
-print('Pizza salami')
+pizza = builder.build()
+print('Pizza salami:')
+pizza.result()
+
+builder = SalamiPizzaBuilder(DoughFirstStrategy())
+director.set_builder(builder)
+director.construct_pizza()
+pizza = builder.build()
 pizza.result()
