0e49b39de433ec268e31611789f27dd8c23a1d58
Vadik <vadumcimbalist@gmail.com>
2024-04-17 12:24:52 +0200

    made strategy for pizza builder

--- Design_Patterns/Strategy/strategy.py
+++ Design_Patterns/Strategy/strategy.py
@@ -6,10 +6,21 @@ class Strategy(ABC):
     def pizza_strategy(self, pizza):
         pass
 
+    def name(self):
+        pass
+
+
 class FillingFirstStrategy(Strategy):
     def pizza_strategy(self, pizza):
         return pizza.filling + [pizza.dough]
 
+    def name(self):
+        return 'FillingFirstStrategy'
+
+
 class DoughFirstStrategy(Strategy):
     def pizza_strategy(self, pizza):
         return [pizza.dough] + pizza.filling
+
+    def name(self):
+        return 'DoughFirstStrategy'
